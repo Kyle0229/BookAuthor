84991ae44a9a1582d40340789af8cc874d070028
kyle <824238868@qq.com>
2019-11-11 10:18:10 +0800

    Initial commit

---
+++ src/main/java/com/kyle/BookAuthorApplication.java
@@ -0,0 +1,14 @@
+package com.kyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationExtensionsKt;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BookAuthorApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BookAuthorApplication.class);
+    }
+}
